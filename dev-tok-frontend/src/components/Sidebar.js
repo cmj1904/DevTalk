6547ec3323551784cb37ca78f9670f18d667ad8f
cmj1980 <dagoclmt@gmail.com>
2022-08-23 07:20:48 -0700

    Added Deps and Sidebar for chat

---
+++ dev-tok-frontend/src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Sidebar.css';
+
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar_header"></div>
+            
+            <div className="sidebar_headerRight">
+             
+            </div>
+            <div className="sidebar_search"></div>
+            <div className="sidebar_chat"></div>
+        </div>
+    )
+};
+
+export default Sidebar
